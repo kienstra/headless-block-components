735d4a3533c252797519638273f1bb582dacc8f6
Ryan Kienstra <kienstraryan@gmail.com>
2020-11-19 21:34:27 -0600

    Get the correct attributes based on which are available
    In the block editor, they
    will be props.attributes,
    and on the front-end they
    are props.attrs

--- src/components/example.js
+++ src/components/example.js
@@ -10,7 +10,8 @@ import { __ } from '@wordpress/i18n'
 
 /**
  * @typedef {Object} ExampleProps
- * @property {Object} attributes The block attributes.
+ * @property {Object} attributes The block attributes available in the block editor.
+ * @property {Object} attrs The block attributes available on the front-end.
  */
 
 /**
@@ -19,17 +20,21 @@ import { __ } from '@wordpress/i18n'
  * @param {ExampleProps} props
  * @return {React.ReactElement} An example preview component.
  */
-const Example = ({ attributes }) => (
-  <div>
-    <p>{__('This is an alternate preview component', 'headless-blocks')}</p>
-    <p>
-      {__(
-        'Here is the value of the new-field attribute, if it exists:',
-        'headless-blocks'
-      )}
-    </p>
-    <p>{attributes['new-field']}</p>
-  </div>
-)
+const Example = (props) => {
+  const attributes = props.attrs ? props.attrs : props.attributes;
+
+  return (
+    <div>
+      <p>{__('This is an alternate preview component', 'headless-blocks')}</p>
+      <p>
+        {__(
+          'Here is the value of the new-field attribute, if it exists:',
+          'headless-blocks'
+        )}
+      </p>
+      <p>{attributes['new-field']}</p>
+    </div>
+  );
+}
 
 export default Example
